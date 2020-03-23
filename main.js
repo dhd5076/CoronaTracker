a53186f73044def318308dd7bca7a3340dd22cf3
Dylan Dunn <dylandunn17@icloud.com>
2020-03-23 12:55:49 -0400

    First Commit, basic working version

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const assetsDirectory = path.join(__dirname, 'assets')
 let tray = undefined
 let window = undefined
 
-// Don't show the app in the doc
+// Don't show the app in the dock
 app.dock.hide()
 
 app.on('ready', () => {
-  createTray()
-  createWindow()
+  createTray();
+  createWindow();
 })
 
 // Quit the app when the window is closed
@@ -49,18 +49,17 @@ const getWindowPosition = () => {
 const createWindow = () => {
   window = new BrowserWindow({
     width: 300,
-    height: 450,
+    height: 150,
     show: false,
     frame: false,
     fullscreenable: false,
     resizable: false,
     transparent: true,
     webPreferences: {
-      // Prevents renderer process code from not running when window is
-      // hidden
+      nodeIntegration: true,
       backgroundThrottling: false
     }
-  })
+  });
   window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
 
   // Hide the window when it loses focus
@@ -68,7 +67,7 @@ const createWindow = () => {
     if (!window.webContents.isDevToolsOpened()) {
       window.hide()
     }
-  })
+  });
 }
 
 const toggleWindow = () => {
@@ -87,38 +86,5 @@ const showWindow = () => {
 }
 
 ipcMain.on('show-window', () => {
-  showWindow()
-})
-
-ipcMain.on('weather-updated', (event, weather) => {
-  // Show "feels like" temperature in tray
-  tray.setTitle(`${Math.round(weather.currently.apparentTemperature)}°`)
-
-  // Show summary and last refresh time as hover tooltip
-  const time = new Date(weather.currently.time).toLocaleTimeString()
-  tray.setToolTip(`${weather.currently.summary} at ${time}`)
-
-  // Update icon for different weather types
-  switch (weather.currently.icon) {
-    case 'cloudy':
-    case 'fog':
-    case 'partly-cloudy-day':
-    case 'partly-cloudy-night':
-      tray.setImage(path.join(assetsDirectory, 'cloudTemplate.png'))
-      break
-    case 'rain':
-    case 'sleet':
-    case 'snow':
-      tray.setImage(path.join(assetsDirectory, 'umbrellaTemplate.png'))
-      break
-    case 'clear-night':
-      tray.setImage(path.join(assetsDirectory, 'moonTemplate.png'))
-      break
-    case 'wind':
-      tray.setImage(path.join(assetsDirectory, 'flagTemplate.png'))
-      break
-    case 'clear-day':
-    default:
-      tray.setImage(path.join(assetsDirectory, 'sunTemplate.png'))
-  }
+  showWindow();
 })
